dd5c928e0201590ecbd719b5d0d0d0bbfdd85818
Jeremy McAnally <jeremymcanally@gmail.com>
2009-01-09 19:33:15 -0600

    Add formatter markup helps to edit/new views.

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -2,4 +2,8 @@ module PagesHelper
   def revert_link_class(commit)
     (commit.message =~ /^rollback/) ? 'rollback' : 'edit'
   end
+
+  def formatter_help
+    render :partial => "shared/formatter_help/#{Formatter.ancestors.first.name.underscore}"
+  end
 end
